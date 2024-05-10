c718d6710b9888c232c6d07f719421388ea4d2bf
marthsincemelee <fin.markwitz@smoothcloud.de>
2024-05-10 20:57:14 +0200

    feat: Basic web service

--- cmd/server.go
+++ cmd/server.go
@@ -2,8 +2,12 @@ package main
 
 //TODO: SQLite starten und dort Infos halten -> einfacherer Dateizugriff
 
-import "server/internal/database"
+import (
+	"server/internal/database"
+	"server/internal/http"
+)
 
 func main() {
 	database.Migrate()
+	http.StartServer()
 }
---
+++ internal/http/rest.go
@@ -0,0 +1,52 @@
+package http
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+	"time"
+)
+
+func StartServer() {
+	r := chi.NewRouter()
+
+	// A good base middleware stack
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	// Set a timeout value on the request context (ctx), that will signal
+	// through ctx.Done() that the request has timed out and further
+	// processing should be stopped.
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Rest-Service is active."))
+	})
+
+	/*	// RESTy routes for "articles" resource
+		r.Route("/articles", func(r chi.Router) {
+			r.With(paginate).Get("/", listArticles)                           // GET /articles
+			r.With(paginate).Get("/{month}-{day}-{year}", listArticlesByDate) // GET /articles/01-16-2017
+
+			r.Post("/", createArticle)       // POST /articles
+			r.Get("/search", searchArticles) // GET /articles/search
+
+			// Regexp url parameters:
+			r.Get("/{articleSlug:[a-z-]+}", getArticleBySlug) // GET /articles/home-is-toronto
+
+			// Subrouters:
+			r.Route("/{articleID}", func(r chi.Router) {
+				r.Use(ArticleCtx)
+				r.Get("/", getArticle)       // GET /articles/123
+				r.Put("/", updateArticle)    // PUT /articles/123
+				r.Delete("/", deleteArticle) // DELETE /articles/123
+			})
+		})
+	*/
+	// Mount the admin sub-router
+	// r.Mount("/admin", adminRouter())
+
+	http.ListenAndServe(":3333", r)
+}
